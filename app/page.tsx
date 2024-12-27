78d9f9d17ae802b5178fefcbb5fcf9856d7e9293
Matteo Omicini <matteoomicini.dev@gmail.com>
2024-12-27 12:59:55 +0100

    fix: responsiveness + minor ui

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             <main
                 className="max-w-7xl container mx-auto p-2 flex flex-col flex-grow gap-8 items-center min-h-[100dvh] w-screen h-full">
                 <FramePanel
-                    className="flex flex-col flex-grow py-10"
+                    className="flex flex-col flex-grow py-5"
                     renderSideHeader={
                         <IconButton
                             size={isDesktop ? 'M' : 'S'}
@@ -74,49 +74,52 @@ export default function Home() {
                             <Button
                                 size={isDesktop ? 'M' : 'S'}
                                 variant="standard"
-                                className={buttonClassNames().largeButton}
+                                className={isDesktop ? buttonClassNames().smallButton : buttonClassNames().largeButton}
                                 text=""
                             />
                             <Button
                                 size={isDesktop ? 'M' : 'S'}
                                 rounded="R"
                                 variant="standard"
-                                className={buttonClassNames().largeButton}
+                                className={buttonClassNames().largeButton + ' w-full'}
                                 text=""
                             />
                         </>
                     }
                 >
                     <div
-                        className={`flex flex-col justify-center ${isDesktop ? 'items-start px-10' : 'items-start px-4'} flex-grow pb-40`}>
-                        <Text variant={isMobile ? 'display2' : "display1"} className="!leading-none">
-                            Nebula Design-System
-                        </Text>
-                        <Text variant={"body1"} className="w-full md:w-3/4 mt-12">
-                            A retro-futuristic design system inspired by 1970s space age style, Bauhaus principles, and
-                            Star
-                            Trek&apos;s
-                            LCARS
-                            interface.
-                        </Text>
-                        <Text variant={"body1"} className="w-full md:w-3/4 mt-5">
-                            A UI kit library actually available for react.js.
-                        </Text>
-                        <div className="flex items-center gap-4 mt-10">
-                            <a href="https://github.com/mattyx96/nebula-ds-react-library" target="_blank">
-                                <Button
-                                    size={isDesktop ? 'L' : 'M'}
-                                    variant="standard"
-                                    text="GitHub"
-                                />
-                            </a>
-                            <a href="https://nebula-ds-react-library.irongalaxy.space" target="_blank">
-                                <Button
-                                    size={isDesktop ? 'L' : 'M'}
-                                    rounded="R"
-                                    text="Explore UI Components"
-                                />
-                            </a>
+                        className={`flex flex-col justify-center ${isDesktop ? 'items-center px-10' : 'items-start px-4'} flex-grow xl:!grid grid-cols-6`}>
+                        <div className="col-span-4 lg:-translate-y-10 2xl:-translate-y-1/3">
+                            <Text variant={isMobile ? 'display2' : "display1"} className="!leading-none">
+                                Nebula Design-System
+                            </Text>
+                            <Text variant={"body1"} className="w-full md:w-3/4 mt-12">
+                                A retro-futuristic design system inspired by 1970s space age style, Bauhaus principles,
+                                and
+                                Star
+                                Trek&apos;s
+                                LCARS
+                                interface.
+                            </Text>
+                            <Text variant={"body1"} className="w-full md:w-3/4 mt-5">
+                                A UI kit library actually available for react.js.
+                            </Text>
+                            <div className="flex items-center gap-4 mt-10">
+                                <a href="https://github.com/mattyx96/nebula-ds-react-library" target="_blank">
+                                    <Button
+                                        size={isDesktop ? 'L' : 'M'}
+                                        variant="standard"
+                                        text="GitHub"
+                                    />
+                                </a>
+                                <a href="https://nebula-ds-react-library.irongalaxy.space" target="_blank">
+                                    <Button
+                                        size={isDesktop ? 'L' : 'M'}
+                                        rounded="R"
+                                        text="Explore UI Components"
+                                    />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </FramePanel>
